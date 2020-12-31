d71b3315bfbd35a4ac6b3da77608b923f0319f5b
Sang <huynhvansang122@gmail.com>
2020-12-31 19:28:43 +0700

    asd

--- javascript/vue.js
+++ javascript/vue.js
@@ -28,18 +28,7 @@ Vue.component('button-counter', {
     }
   },
   
-  watch:
-  {
-      // isCheck:function()
-      // {
-      //   if(this.isCheck ==true)
-      //   {
 
-      //     console.log("ok");
-      //     this.correcAnswertClass='correctAnswerClass'
-      //   }
-      // }
-  },
    methods:
     {
       hiddenCard()
@@ -138,28 +127,6 @@ Vue.component('button-counter', {
       </div>
     </div>    
 `
-   
-//    `
-   
-//    <div class="scroll-cards__item" >
-//     <p>{{cauhoi.question}}</p>
-//     <button
-//       v-for="item in dapan"  --bind dap an
-//       @click.prevent="selectAnswer(item)" -- mỗi dap an la 1 cai index
-//       v-bind:class="AnswerClass(item)"  -- add class cho button duoc click
-//     >
-//       {{item}}
-//     </button> 
-    // <div>
-    //     <button
-    //     @click="checkAnswer(),destroy()"
-    //     :disabled="selectIndex === null " // cho submit khi chon 1 dap an
-    //     >submit</button>                   // luc đó điều kiện disabled bị sai nên submit được
-
-    //   </div>
-//     </div>    
-// `
-
 
 
 
@@ -169,8 +136,17 @@ var question = new Vue({
 	el:"#questionComponent",
 	data: {
 
-   		StoreQuestion:[],// bien luu cau hoi
-   		Answers:[],//bien luu dap an [ [],[] ]
+   		StoreQuestion:
+            [
+                {correct: 3,question:'What is my favorite'},
+                {correct: 2,question:'Hello dog'},
+                {correct: 1,question:'Hello chicken'}
+            ],// bien luu cau hoi
+   		Answers:[
+   		    ['chicken','meat','rice','beef'],
+            ['ok','gau','ang','cmm'],
+            ['q','lo','zo','ae']
+        ],//bien luu dap an [ [],[] ]
       numCorrect:0,
       TotalSubmit:0
       
@@ -186,51 +162,5 @@ var question = new Vue({
         }
         this.TotalSubmit++;
       }
-    },
-
-   
-  	mounted()//tao data va ket qua
-  	{
-  		for(let i=0;i<10;i++)
-			{
-				let num1 =Math.floor(Math.random() * 1000); 
-				let num2 =Math.floor(Math.random() * 100);
-				operator= ['+','-','*','/'];
-		        ran = (Math.floor(Math.random() * 10))%4
-		        let c= num1+operator[ran]+num2; 
-		        //3 + 4
-		        let result =eval(c);	
-
-        b=(Math.floor(Math.random() * 10))%4;
-          //random vị trí đáp án
-            
-            let re0=0 ,re1=1,re2=2,re3=3;
-          
-            if(b==3)
-            {
-              re0=result-10 ,re1=result+10,re2=result+20,re3=result;
-            }
-            if(b==2)
-            {
-              re0=result-10 ,re1=result+10,re2=result,re3=result+20;
-            }
-            if(b==1)
-            {
-               re0=result+20 ,re1=result,re2=result-10,re3=result+10;
-            }
-            if(b==0)
-            {
-              re0=result ,re2=result+10,re3=+20,re1=-10;
-                    
-            }
-
-
-
-
-		        this.StoreQuestion.push({correct:b,question:c }) 
-            this.Answers.push([re0,re1,re2,re3])
-			}
-  	}
-
-
+    }
 });
