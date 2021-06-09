109713151ed2b08fdd82c6d618d91bd9b4482fdb
Sang <huynhvansang122@gmail.com>
2021-06-09 16:39:21 +0700

    add cp

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8fe9634cb863e744cffd6a5347c2c93833094c31',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8fe9634cb863e744cffd6a5347c2c93833094c31',
+    ),
+  ),
+);
